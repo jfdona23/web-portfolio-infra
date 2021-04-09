d85a93528f083c61dc912291fd933687e60b5d5b
Juan Fernando Donadio <jf.donadio@gmail.com>
2021-04-09 14:24:33 -0300

    Added S3 backend support

--- terraform/base/provider.tf
+++ terraform/base/provider.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = var.region
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "jfdonadio-tf-state"
+    region         = "us-east-1"
+    # dynamodb_table = "jfdonadio-terraform-locks"
+    key            = "web-portfolio-base/terraform.tfstate"
+  }
+}
